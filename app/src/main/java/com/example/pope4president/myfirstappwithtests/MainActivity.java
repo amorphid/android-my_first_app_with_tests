70d5f9bd755ad070a2c6df132ad31206f16c5e88
Michael Pope <mpope.cr@gmail.com>
2015-02-03 11:57:52 -0500

    Add displayed message for button send

--- app/src/main/java/com/example/pope4president/myfirstappwithtests/MainActivity.java
+++ app/src/main/java/com/example/pope4president/myfirstappwithtests/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.pope4president.myfirstappwithtests;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "com.example.pope4president.myfirstappwithtests.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,13 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
