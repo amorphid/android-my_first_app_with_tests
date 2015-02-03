af4dad40c0c9267096b29fa5050aaa51f76af199
Michael Pope <mpope.cr@gmail.com>
2015-02-03 11:19:34 -0500

    Add Send Button text test

--- app/src/androidTest/java/com/example/pope4president/myfirstappwithtests/HelloWorldEspressoTest.java
+++ app/src/androidTest/java/com/example/pope4president/myfirstappwithtests/HelloWorldEspressoTest.java
@@ -6,6 +6,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class HelloWorldEspressoTest extends ActivityInstrumentationTestCase2<MainActivity>{
     public HelloWorldEspressoTest(){
@@ -20,5 +21,9 @@ public class HelloWorldEspressoTest extends ActivityInstrumentationTestCase2<Mai
 
     public void testSendButtonDisplays() {
         onView(withId(R.id.button_send)).check(matches(isDisplayed()));
+    }
+
+    public void testSendButtonText() {
+        onView(withId(R.id.button_send)).check(matches(withText("Press Me")));
     }
 }
