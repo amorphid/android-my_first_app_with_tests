3e2a9d2cc42982cbfca1fee769cea499fd260d4f
Michael Pope <mpope.cr@gmail.com>
2015-02-03 11:10:00 -0500

    Add Send Button w/ tests

--- app/src/androidTest/java/com/example/pope4president/myfirstappwithtests/HelloWorldEspressoTest.java
+++ app/src/androidTest/java/com/example/pope4president/myfirstappwithtests/HelloWorldEspressoTest.java
@@ -4,8 +4,8 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class HelloWorldEspressoTest extends ActivityInstrumentationTestCase2<MainActivity>{
     public HelloWorldEspressoTest(){
@@ -18,7 +18,7 @@ public class HelloWorldEspressoTest extends ActivityInstrumentationTestCase2<Mai
         getActivity();
     }
 
-    public void testListGoesOverTheFold() {
-        onView(withId(R.id.hello_world)).check(matches(withText(R.string.hello_world)));
+    public void testSendButtonDisplays() {
+        onView(withId(R.id.button_send)).check(matches(isDisplayed()));
     }
 }
