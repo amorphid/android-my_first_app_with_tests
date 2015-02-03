5e114d69726cde091dc8d471069fa377f21f6df9
Michael Pope <mpope.cr@gmail.com>
2015-02-03 13:22:14 -0500

    Refactor

--- app/src/androidTest/java/com/example/pope4president/myfirstappwithtests/PressMeButtonTest.java
+++ app/src/androidTest/java/com/example/pope4president/myfirstappwithtests/PressMeButtonTest.java
@@ -1,5 +1,6 @@
 package com.example.pope4president.myfirstappwithtests;
 
+import android.support.test.espresso.ViewInteraction;
 import android.test.ActivityInstrumentationTestCase2;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -11,6 +12,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class PressMeButtonTest extends ActivityInstrumentationTestCase2<MainActivity>{
+
+
     public PressMeButtonTest(){
         super(MainActivity.class);
     }
@@ -21,17 +24,22 @@ public class PressMeButtonTest extends ActivityInstrumentationTestCase2<MainActi
         getActivity();
     }
 
+    public ViewInteraction subject() {
+        return onView(withId(R.id.button_send));
+    }
+
     public void testIsDisplayed() {
-        onView(withId(R.id.button_send)).check(matches(isDisplayed()));
+        subject().check(matches(isDisplayed()));
     }
 
     public void testDisplaysButtonText() {
-        onView(withId(R.id.button_send)).check(matches(withText("Press Me")));
+        subject().check(matches(withText("Press Me")));
     }
 
     public void testMessageSend(){
-        onView(withId(R.id.edit_message)).perform(typeText("Hello"));
-        onView( withId(R.id.button_send)).perform(click());
-        onView(withId(R.id.text_sent)).check(matches(withText("Hello")));
+        String text = "hello";
+        onView(withId(R.id.edit_message)).perform(typeText(text));
+        subject().perform(click());
+        onView(withId(R.id.text_sent)).check(matches(withText(text)));
     }
 }
